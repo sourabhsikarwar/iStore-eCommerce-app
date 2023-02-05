97ce8d5ae837550face5e7fb7ffb0898234fa361
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2023-02-05 12:00:22 +0530

    Stripe payment added in backend

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,25 @@
 const express = require("express")
+const cors = require("cors")
 const { connectDB } = require("./config/dbConn")
 const userRoute = require("./routes/user")
 const productRoute = require("./routes/product")
 const authRoute = require("./routes/auth")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
 
 const PORT = 5000 || process.env.PORT
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/product", productRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/checkout", stripeRoute)
 
 const startServer = () => {
     connectDB()
--- Server/routes/order.js
+++ Server/routes/order.js
@@ -12,37 +12,37 @@ dotenv.config();
 
 // create order
 router.post("/", verifyToken, async (req, res) => {
-    const newOrder = new Order(req.body)
+  const newOrder = new Order(req.body);
 
-    try{
-        const savedOrder = await newOrder.save()
-        res.status(201).json(savedOrder)
-    } catch(error){
-        res.status(500).json(error)
-    }
-})
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(201).json(savedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // get user orders
 router.get("/find/:userId", verifyTokenAndAuth, async (req, res) => {
-    try{
-        const orders = await Order.find({userId: req.params.userId})
-        res.status(200).json(orders)
-    } catch(error) {
-        res.status(500).json(error)
-    }
-})
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // get all orders only by admin
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
-    try {
-        const orders = await Order.find()
-        res.status(200).json(orders)
-    } catch(error) {
-        res.status(500).json(error)
-    }
-})
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-// update order 
+// update order
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const updatedOrder = await Order.findByIdAndUpdate(
@@ -60,12 +60,42 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 
 // delete order
 router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-    try{
-        await Order.findByIdAndDelete(req.params.id)
-        res.status(200).json("Order has been deleted!")
-    } catch(error){
-        res.status(500).json(error)
-    }
-})
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted!");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get monthly income
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const prevMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      {
+        $match: { createdAt: { $gte: prevMonth } },
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
